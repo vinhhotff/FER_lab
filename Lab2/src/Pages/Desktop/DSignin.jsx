ad08ca4f583d7c98f690413b6008caff9af463ed
HoangKhuong2807 <pchkhuong123@gmail.com>
2025-02-17 08:20:53 +0700

    lab2

--- Lab2/src/Pages/Desktop/DSignin.jsx
+++ Lab2/src/Pages/Desktop/DSignin.jsx
@@ -1,6 +1,5 @@
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/Label";
+import { Label } from "@/components/ui/label";
 
 function Signin() {
   return (
--- Lab2/src/Pages/Mobile/MSignin.jsx
+++ Lab2/src/Pages/Mobile/MSignin.jsx
@@ -1,7 +1,5 @@
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/Label";
-
+import { Label } from "@/components/ui/label";
 
 export default function MSignin(){
         return(
